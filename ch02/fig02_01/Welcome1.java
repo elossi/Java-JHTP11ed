ff37fbaea1e24213001fe35cf2fd8fb92b2d7905
Esko Lossi <61895324+elossi@users.noreply.github.com>
2020-06-27 16:52:04 +1000

    Add files via upload

---
+++ ch02/fig02_01/Welcome1.java
@@ -0,0 +1,11 @@
+// Fig. 2.1: Welcome1.java
+// Text-printing program.
+
+public class Welcome1 {
+   // main method begins execution of Java application
+   public static void main(String[] args) {
+      System.out.println("Hello 2020 ");
+     // System.out.println("Java \nProgramming!");
+   } // end method main
+} // end class Welcome1
+
